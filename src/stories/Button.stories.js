69ff438fe2b622fabe1ca8d92fb186ffad4d227a
Denys Rocha <contato.denysrocha@gmail.com>
2024-03-26 18:41:15 -0300

    Fixing bugs in button stories

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,14 +1,16 @@
 import { fn } from '@storybook/test';
-import MyButton from '@/components/Button.vue';
+import PocButton from '@/components/Button.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
-  title: 'Example/Button',
-  component: MyButton,
+  title: 'Example/PocButton',
+  component: PocButton,
   tags: ['autodocs'],
   argTypes: {
     size: { control: { type: 'select' }, options: ['small', 'medium', 'large'] },
+    type: { control: { type: 'select' }, options: ['primary', 'secondary', 'tertiary', 'danger'] },
     backgroundColor: { control: 'color' },
+    disabled: { control: { type: 'boolean' }}
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -17,13 +19,27 @@ export default {
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary = {
   args: {
-    primary: true,
     label: 'Button',
   },
 };
 
 export const Secondary = {
   args: {
+    type: 'secondary',
+    label: 'Button',
+  },
+};
+
+export const Tertiary = {
+  args: {
+    type: 'tertiary',
+    label: 'Button',
+  },
+};
+
+export const Danger = {
+  args: {
+    type: 'danger',
     label: 'Button',
   },
 };
